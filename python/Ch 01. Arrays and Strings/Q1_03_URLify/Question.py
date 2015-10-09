f1ed304b724764371273a7a519102a0e621bfbd5
michaelchum <michaelhochum@gmail.com>
2015-10-08 23:26:43 -0700

    Added classical solution to Chap 1 Q3

--- python/Ch 01. Arrays and Strings/Q1_03_URLify/Question.py
+++ python/Ch 01. Arrays and Strings/Q1_03_URLify/Question.py
@@ -7,6 +7,18 @@ def replace_space(input_str, length):
   return input_str.replace(" ", "%20")
 
 # Using char replacement
+def replace_array(input_str, length):
+  """
+  >>> replace_space("Mr John Smith", 13)
+  'Mr%20John%20Smith'
+  """
+  result = []
+  for letter in input_str:
+    if letter is not " ":
+      result.append(letter)
+    else:
+      result.append('%20')
+  return result
 
 if __name__ == '__main__':
     import doctest
